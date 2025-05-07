6480e97bf68f6ee9ecd4feff14d680a612056d1f
Ohmin Kwon <ohminkwon2718@gmail.com>
2025-05-08 01:24:29 +0900

    세부창 이쁘게 변경

--- presentation/src/main/java/com/kintmin/presentation/ui/audio_media_detail/AudioMediaDetailScreen.kt
+++ presentation/src/main/java/com/kintmin/presentation/ui/audio_media_detail/AudioMediaDetailScreen.kt
@@ -1,28 +1,59 @@
 package com.kintmin.presentation.ui.audio_media_detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBackIosNew
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.kintmin.presentation.theme.JellyTubeTheme
+import java.io.File
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AudioMediaDetailScreen(
     navigateToBack: () -> Unit,
 ) {
+    val imageRequest = ImageRequest.Builder(LocalContext.current)
+        .data(
+            androidx.media3.session.R.drawable.media3_icon_artist
+        )
+        .memoryCachePolicy(coil.request.CachePolicy.ENABLED)
+        .diskCachePolicy(coil.request.CachePolicy.DISABLED)
+        .build()
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -46,12 +77,117 @@ fun AudioMediaDetailScreen(
             )
         },
     ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            AsyncImage(
+                model = imageRequest,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+                    .height(220.dp)
+                    .background(Color.Gray)
+            )
+        }
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
+                .padding(top = innerPadding.calculateTopPadding() + 210.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(color = Color.White),
         ) {
+            Text(
+                modifier = Modifier.padding(top = 20.dp, start = 16.dp, bottom = 8.dp),
+                text = "미디어 이름",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                lineHeight = 10.sp,
+            )
+            Text(
+                modifier = Modifier.padding(start = 16.dp, bottom = 4.dp),
+                text = "아티스트: 나는야 아티스트",
+                fontSize = 16.sp,
+                maxLines = 1,
+                lineHeight = 1.sp,
+            )
+            Text(
+                modifier = Modifier.padding(start = 16.dp, bottom = 4.dp),
+                text = "재생 시간: 999:99",
+                fontSize = 16.sp,
+            )
+            Text(
+                modifier = Modifier.padding(start = 16.dp, bottom = 4.dp),
+                text = "생성 시각: yyyy년 M월 dd일 HH:mm",
+                fontSize = 16.sp,
+            )
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = "출처: https://youtube.com",
+                fontSize = 16.sp,
+            )
 
+            Spacer(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth()
+                    .height(0.5.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color.Gray)
+            )
+
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = "플레이리스트 이름",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(
+                modifier = Modifier.padding(top = 8.dp, start = 16.dp),
+                text = "생성된 시각: yyyy년 M월 dd일 HH:mm",
+                fontSize = 15.sp,
+            )
+            Text(
+                modifier = Modifier.padding(top = 4.dp, start = 16.dp),
+                text = "추가된 시각: yyyy년 M월 dd일 HH:mm",
+                fontSize = 15.sp,
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth()
+                    .height(0.5.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color.Gray)
+            )
+
+            Text(
+                modifier = Modifier.padding(start = 16.dp, bottom = 16.dp, end = 16.dp),
+                text = "이것은 미디어 설명입니다. 이것은 미디어 설명입니다. 이것은 미디어 설명입니다. 이것은 미디어 설명입니다. 이것은 미디어 설명입니다.",
+                fontSize = 16.sp,
+            )
+
+            Row(
+                modifier = Modifier.weight(1f),
+                verticalAlignment = Alignment.Bottom
+            ) {
+                TextButton(
+                    modifier = Modifier.weight(1f).height(56.dp),
+                    onClick = {}) {
+                    Text(
+                        text = "삭제",
+                        fontSize = 16.sp,
+                    )
+                }
+                TextButton(
+                    modifier = Modifier.weight(1f).height(56.dp),
+                    onClick = {}) {
+                    Text(
+                        text = "수정",
+                        fontSize = 16.sp,
+                    )
+                }
+            }
         }
     }
 }
