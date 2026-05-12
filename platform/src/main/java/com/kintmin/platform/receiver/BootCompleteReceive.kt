26e46572a12a528f9a4bea0438bec5e4607d7303
Ohmin Kwon <ohminkwon2718@gmail.com>
2026-05-12 16:04:03 +0900

    feature/xml 정리 및 필요한 주석 추가

--- platform/src/main/java/com/kintmin/platform/receiver/BootCompleteReceive.kt
+++ platform/src/main/java/com/kintmin/platform/receiver/BootCompleteReceive.kt
@@ -5,14 +5,16 @@ import android.content.Context
 import android.content.Intent
 import com.kintmin.platform.service.StepForegroundService
 
+/**
+ * 참고사항: ACTION_BOOT_COMPLETED 백그라운드에서도 Foreground 실행 가능
+ * https://developer.android.com/develop/background-work/services/fgs/restrictions-bg-start#background-start-restriction-exemptions
+ */
 class BootCompleteReceive : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
         when(intent?.action) {
             Intent.ACTION_BOOT_COMPLETED -> {
-                runCatching {
-                    StepForegroundService.startService(context)
-                }
+                StepForegroundService.startService(context)
             }
         }
     }
--- platform/src/main/java/com/kintmin/platform/receiver/PackageChangedReceiver.kt
+++ platform/src/main/java/com/kintmin/platform/receiver/PackageChangedReceiver.kt
@@ -5,14 +5,16 @@ import android.content.Context
 import android.content.Intent
 import com.kintmin.platform.service.StepForegroundService
 
+/**
+ * 참고사항: MY_PACKAGE_REPLACED 백그라운드에서도 Foreground 실행 가능
+ * https://developer.android.com/develop/background-work/services/fgs/restrictions-bg-start#background-start-restriction-exemptions
+ */
 class PackageChangedReceiver: BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
         when(intent?.action) {
             Intent.ACTION_MY_PACKAGE_REPLACED -> {
-                runCatching {
-                    StepForegroundService.startService(context)
-                }
+                StepForegroundService.startService(context)
             }
         }
     }
